dc8514224ea98ee6c2ed3587d35467f7e65c6078
Aaron Ky-Riesenbach <aaronkyriesenbach@pobox.com>
2023-01-19 10:41:52 -0500

    Remove sleep/login configuration (to be added back as drop-ins)

--- .config/aconfmgr/10-base.sh
+++ .config/aconfmgr/10-base.sh
@@ -72,10 +72,8 @@ CopyFile /etc/ly/config.ini
 CopyFile /etc/polkit-1/rules.d/10-manage-openvpn.rules
 CopyFile /etc/polkit-1/rules.d/20-manage-reflector.rules
 CopyFile /etc/sysctl.d/20-quiet-printk.conf
-CopyFile /etc/systemd/logind.conf
 CopyFile /etc/systemd/network/10-wired.network
 CopyFile /etc/systemd/network/20-wireless.network
 CopyFile /etc/systemd/resolved.conf
-CopyFile /etc/systemd/sleep.conf
 
 CreateLink /etc/udev/rules.d/80-net-setup-link.rules /dev/null
