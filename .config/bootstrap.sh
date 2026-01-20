b791f7a97d3eaa99885f2c2945c38c77393728ca
Aaron Ky-Riesenbach <aaron@kyriesenba.ch>
2026-01-20 13:05:09 -0500

    Add missing apps to bootstrap

--- .config/bootstrap.sh
+++ .config/bootstrap.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-sudo pacman -S sway swaylock swayidle swaybg waybar sway-contrib git iwd ttf-iosevka-nerd ttf-liberation sudo base-devel less fd mako vim neovim brightnessctl
+sudo pacman -S sway swaylock swayidle swaybg waybar sway-contrib git iwd ttf-iosevka-nerd ttf-liberation sudo base-devel less fd mako vim neovim brightnessctl direnv kubectl starship foot
 
 
 git clone https://aur.archlinux.org/paru.git
 cd paru
 makepkg -si
 
-paru -S avizo
+paru -S avizo sway-launcher-desktop
